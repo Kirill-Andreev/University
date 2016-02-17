f693d3b814cf82354d357e9a7bc92c569dd5f66e
Kirill-Andreev <andreev.kirill102@gmail.com>
2016-02-17 14:35:02 +0300

    Iterative and recursive factorial
    Implementation of iterative and recursive factorial

---
+++ Homework_1/Task_1/Task_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        private static int RecursiveFactorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n * RecursiveFactorial(n - 1);
+        }
+
+        private static int IterativeFactorial(int n)
+        {
+            int result = 1;
+            if (n == 0)
+            {
+                return 1;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            for (int i = 1; i <= n; ++i)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter the number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(RecursiveFactorial(number));
+            Console.WriteLine(IterativeFactorial(number));
+        }
+    }
+}
