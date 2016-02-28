4a042d088b73fd0d56a5d4f6ccceb26ea6d37e51
Kirill-Andreev <andreev.kirill102@gmail.com>
2016-02-28 14:33:49 +0300

    Implementation of stack

---
+++ Homework_2/Task_1/Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ArrayStackNameSpace;
+
+namespace Task_1
+{
+    /// <summary>
+    /// Main class of the program
+    /// displays the contents of the stack,
+    /// filled with numbers in a cycle
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The entry point of the program
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            ArrayStack stack = new ArrayStack();
+            for (int i = 0; !stack.IsOverflowed(); ++i)
+            {
+                stack.Push(i);
+            }
+
+            for (int i = 0; !stack.IsEmpty(); ++i)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+        }
+    }
+}
