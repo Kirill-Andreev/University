7cc7e48b75434cd8761233b5536f0e0afd7e3ef8
Kirill-Andreev <andreev.kirill102@gmail.com>
2016-03-20 15:45:33 +0300

    Modified hash table

---
+++ Homework_3/Task_2/Task_2/HashFunction1.cs
@@ -0,0 +1,19 @@
+﻿namespace HashTableNameSpace
+{
+    /// <summary>
+    /// First hash function class
+    /// </summary>
+    public class HashFunction1 : IHashFunction
+    {
+        /// <summary>
+        /// Returns an integer hash value
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="hashConst"></param>
+        /// <returns></returns>
+        public int HashFunction(int element, int hashConst)
+        {
+            return element % hashConst;
+        }
+    }
+}
