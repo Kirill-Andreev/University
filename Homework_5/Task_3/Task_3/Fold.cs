9969aadf085654a57bfe0a975e1f306a011f7fbf
Kirill-Andreev <andreev.kirill102@gmail.com>
2016-03-25 13:45:28 +0300

    Not finish

---
+++ Homework_5/Task_3/Task_3/Fold.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoldNameSpace
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Fold
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="initialValue"></param>
+        /// <param name="function"></param>
+        /// <returns></returns>
+        public int FoldFunction(List<int> list, int initialValue, Func<int, int, int> function)
+        {
+            int store = initialValue;
+            for (int i = 0; i < list.Count; ++i)
+            {
+                store = function(store, list[i]);
+            }
+            return store;
+        }
+    }
+}
---
+++ Homework_5/Task_3/Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoldNameSpace
+{
+    /// <summary>
+    /// Main class
+    /// displays the accumulated value
+    /// after using function
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The entry point of the program
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Fold fold = new Fold();
+            //fold.FoldFunction(new List<int>() { 1, 2, 3 }, 1, (acc, elem) => acc * elem);
+
+            Console.WriteLine(fold.FoldFunction(new List<int>() { 1, 2, 3 }, 1, (acc, elem) => acc * elem));
+        }
+    }
+}
