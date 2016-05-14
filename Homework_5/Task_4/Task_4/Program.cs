9a5508c32627c7f5805c47b05227c994c00eee63
Kirill-Andreev <andreev.kirill102@gmail.com>
2016-05-14 17:23:38 +0300

    EventLoop

---
+++ Homework_5/Task_4/Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EventLoopNameSpace
+{
+    /// <summary>
+    /// Main class of the program
+    /// that triggers an event and starts the event loop
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var eventLoop = new EventLoop();
+            var moveCursor = new MoveCursor();
+            eventLoop.LeftHandler += moveCursor.Left;
+            eventLoop.RightHandler += moveCursor.Right;
+            eventLoop.UpHandler += moveCursor.Up;
+            eventLoop.DownHandler += moveCursor.Down;
+            eventLoop.Run();
+        }
+    }
+}
