df1b15fa2bf53da4f1aa95970a7aaeba42a8856a
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-03-22 19:12:25 +0300

    Implementation of function that reverse the list

---
+++ 4th semester/ReverseList/ReverseList/Program.fs
@@ -0,0 +1,9 @@
+﻿let reverseList list = 
+     let rec reverse list acc =
+        match list with
+        | head :: tail -> reverse tail (head :: acc)
+        | [] -> acc
+     reverse list []
+
+let list = [1; 2; 3; 4; 5]
+printf "reverse list: %A" <| reverseList list
