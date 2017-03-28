c0344bc1216261f1fbc643f8ff6612053aceca86
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-03-28 22:43:25 +0300

    Add 2.3

---
+++ 4th semester/Homework_2/Palindrome/Palindrome/Program.fs
@@ -0,0 +1,14 @@
+﻿let rec checkPalindrom (str : string) =
+    let size = str.Length
+    match size with
+    | 0 |  1 -> true
+    | _ -> if (str.[0] = ' ') then checkPalindrom str.[1 .. size - 1]
+           else if (str.[size - 1] = ' ') then checkPalindrom str.[0 .. size - 2]
+           else if (str.[0] <> str.[size - 1] ) then false
+           else checkPalindrom str.[1 .. size - 2]
+
+let str = "a roza upala na lapu azora"
+printf "str is palindrome: %b \n" <| checkPalindrom str
+
+let str1 = "mama mila ramu"
+printf "str1 is palindrome: %b \n" <| checkPalindrom str1
