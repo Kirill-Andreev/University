491383a39ba4768bd344f51302caf8dccb02b0ce
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-03-31 18:46:18 +0300

    Add 3.2

---
+++ 4th semester/Homework_3/MaxSum/MaxSum/Program.fs
@@ -0,0 +1,8 @@
+﻿let positionOfMaxSum list =
+    let tupleList = List.zip (0 :: list) (list @ [0]) 
+    let listSumTuple = List.map (fun (x, y) -> x + y) tupleList
+    let listMax = List.max listSumTuple
+
+    List.findIndex ( (=) listMax ) listSumTuple
+
+printfn "Position: %d \n" <| positionOfMaxSum [1; 5; 6; 2] 
