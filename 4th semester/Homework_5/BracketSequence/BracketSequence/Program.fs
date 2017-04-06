d855efc0514355fbc36e05ba3e92b760bb0b5ad7
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-04-06 15:03:10 +0300

    Add 5.1

---
+++ 4th semester/Homework_5/BracketSequence/BracketSequence/Program.fs
@@ -0,0 +1,27 @@
+﻿let isCorrect string = 
+    let rec aux string position checkList =
+        if (position <> String.length string) then
+           if (string.[position] = '{' || string.[position] = '(' || string.[position] = '[') then
+              let checkList = string.[position] :: checkList;
+              let position = position + 1;
+              aux string position checkList
+           else
+              if (string.[position] = ']' && List.head checkList <> '[' || string.[position] = '}' && List.head checkList <> '{' || string.[position] = ')' && List.head checkList <> '(') then
+                 false;
+              else 
+                 let checkList = List.tail checkList;
+                 let position = position + 1;
+                 if (position >= String.length string && List.length checkList = 0) then 
+                    true;
+                 else    
+                    aux string position checkList
+        else 
+           if (List.length checkList = 0) then
+              true;
+            else 
+              false;
+    aux string 0 [];
+
+printf("Sequence is correct: %b \n") <| isCorrect "({[]})"
+printf("Sequence is correct: %b \n") <| isCorrect "({])"
+printf("Sequence is correct: %b \n") <| isCorrect ""
