7997f7866c7a28f42c17d8da2c88cf0a0d8db515
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-04-06 14:30:40 +0300

    Add 4.4

---
+++ 4th semester/Homework_4/PrimeNumbers/PrimeNumbers/Program.fs
@@ -0,0 +1,4 @@
+﻿let isPrime n = not ((Seq.exists (fun x -> n % x = 0)) (seq {2 .. n / 2}))
+let primeNumbers = Seq.filter isPrime (Seq.initInfinite (fun x -> x + 2))
+  
+printfn "%A" (primeNumbers)
