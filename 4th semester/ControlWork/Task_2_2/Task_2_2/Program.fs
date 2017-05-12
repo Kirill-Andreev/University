b3a4ecafc113e662464baf7c6137697b14475e43
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-05-12 11:17:13 +0300

    Some fixes

--- 4th semester/ControlWork/Task_2_2/Task_2_2/Program.fs
+++ 4th semester/ControlWork/Task_2_2/Task_2_2/Program.fs
@@ -4,9 +4,9 @@ let sinAverage list =
     let rec aux list temp length= 
         match list with
         | [] -> temp / length
-        | head :: tail -> aux tail (temp + head) (length + 1.0)
+        | head :: tail -> aux tail (temp + sin head) (length + 1.0)
     aux list 0.0 0.0
 
-let list = [sin 1.0; sin 2.0; sin 3.0]
+let list = [1.0; 2.0; 3.0]
 let avg =  sinAverage list
 printf "%f\n" avg 
