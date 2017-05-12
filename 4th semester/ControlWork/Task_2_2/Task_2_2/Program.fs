0b5130e6618988fc578f1e3184c48395cb7f23cd
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-05-12 11:00:45 +0300

    Add Test 1.1

---
+++ 4th semester/ControlWork/Task_2_2/Task_2_2/Program.fs
@@ -0,0 +1,12 @@
+﻿module Task_2
+
+let sinAverage list = 
+    let rec aux list temp length= 
+        match list with
+        | [] -> temp / length
+        | head :: tail -> aux tail (temp + head) (length + 1.0)
+    aux list 0.0 0.0
+
+let list = [sin 1.0; sin 2.0; sin 3.0]
+let avg =  sinAverage list
+printf "%f\n" avg 
