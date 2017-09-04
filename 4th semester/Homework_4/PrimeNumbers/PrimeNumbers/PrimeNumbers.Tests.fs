574ea0df24389e7e3238baf120e7e5ffa4b8f510
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-09-04 22:59:34 +0300

    Add tests

---
+++ 4th semester/Homework_4/PrimeNumbers/PrimeNumbers/PrimeNumbers.Tests.fs
@@ -0,0 +1,10 @@
+﻿module PrimeNumbers.Tests
+
+open Program
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+    let ``test isPrime function`` () =
+        isPrime 4 |> should equal false
+        isPrime 5 |> should equal true
--- 4th semester/Homework_4/PrimeNumbers/PrimeNumbers/Program.fs
+++ 4th semester/Homework_4/PrimeNumbers/PrimeNumbers/Program.fs
@@ -1,4 +1,6 @@
-﻿let isPrime n = not ((Seq.exists (fun x -> n % x = 0)) (seq {2 .. n / 2}))
+﻿module Program
+
+let isPrime n = not ((Seq.exists (fun x -> n % x = 0)) (seq {2 .. n / 2}))
 let primeNumbers = Seq.filter isPrime (Seq.initInfinite (fun x -> x + 2))
   
 printfn "%A" (primeNumbers)
