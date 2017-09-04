8ded1ee5d40d7a36a1a627f7234e3cc52234f8a0
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-09-05 00:20:21 +0300

    Add 4.2

---
+++ 4th semester/Homework_4/TreeMap/TreeMap/Program.fs
@@ -0,0 +1,19 @@
+﻿module Program
+
+type BinaryTree<'a> = 
+    | Tree of 'a * BinaryTree<'a> * BinaryTree<'a>
+    | Tip of 'a
+
+let rec treeMap tree func =
+    match tree with 
+    | Tree(x, left, right) -> Tree(func x, treeMap left func, treeMap right func)
+    | Tip x -> Tip(func x) 
+
+let testTree = 
+    Tree(
+        3, 
+        Tree(2, Tip(4), Tip(5)),
+        Tree(4, Tip(5), Tip(7))
+    )
+
+printfn "%A" <| treeMap testTree (fun x -> 2 * x)
---
+++ 4th semester/Homework_4/TreeMap/TreeMap/TreeMap.Tests.fs
@@ -0,0 +1,21 @@
+﻿module TreeMap.Tests
+
+open Program
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+    let ``standart test`` () =
+        let testTree = 
+                Tree(
+                    3, 
+                    Tree(2, Tip(4), Tip(5)), 
+                    Tree(4, Tip(5), Tip(7))
+                )
+        let doubledTree =
+                Tree(
+                    6, 
+                    Tree(4, Tip(8), Tip(10)), 
+                    Tree(8, Tip(10), Tip(14))
+                )
+        treeMap testTree (fun x -> 2 * x) |> should equal doubledTree
