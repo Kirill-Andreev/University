c51665825d0657acdd08f80d389ab4033bf2978d
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-09-05 09:10:27 +0300

    Add 4.3

---
+++ 4th semester/Homework_4/ParseTree/ParseTree/Program.fs
@@ -0,0 +1,30 @@
+﻿module Program
+
+type Operation =
+    | Add
+    | Sub
+    | Mult
+    | Div
+
+type ParseTree =
+    | Node of Operation * ParseTree * ParseTree
+    | Tip of int
+
+let rec parse tree =
+    match tree with
+    | Tip tip -> tip
+    | Node (operation, left, right) ->
+        match operation with
+        | Add -> parse left + parse right
+        | Sub -> parse left - parse right
+        | Mult -> parse left * parse right
+        | Div -> parse left / parse right
+
+let testTree = 
+     Node(
+         Mult, 
+         Node(Add, Tip(4), Tip(5)),
+         Node(Sub, Tip(5), Tip(7))
+     )
+
+printf "%d" <| parse testTree
