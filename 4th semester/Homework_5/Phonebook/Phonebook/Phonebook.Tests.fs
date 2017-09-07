b3d36d4bf7942caca0736ab5506d67cd61746b8f
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-09-07 19:05:29 +0300

    Some fixes

--- 4th semester/Homework_5/Phonebook/Phonebook/Phonebook.Tests.fs
+++ 4th semester/Homework_5/Phonebook/Phonebook/Phonebook.Tests.fs
@@ -13,8 +13,3 @@ open FsUnit
     let ``add new record test`` () =
         let inputData = [("Kirill", "1111")]
         addRecord "Andreev" "2222" inputData |> should equal [("Andreev", "2222"); ("Kirill", "1111")]
-
-[<Test>]
-    let ``find number by name test`` () =
-        let inputData = [("Andreev", "2222"); ("Kirill", "1111")]
-        findNumber "Kirill" inputData |> should equal true
