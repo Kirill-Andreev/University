5c6e82606adb259c292a32b306db4b938ad32f94
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-10-09 01:58:56 +0300

    Add 7.1

---
+++ 4th semester/Homework_7/RoundingBuilder/RoundingBuilder/Program.fs
@@ -0,0 +1,16 @@
+﻿module Program
+
+type RoundingBuilder(precision : int) =
+    member this.Bind (x : float, result : float -> float) =
+        System.Math.Round(result x, precision)
+
+    member this.Return (x : float) = x
+
+let rounding precision = RoundingBuilder precision
+
+let calculate precision =
+    rounding precision {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
---
+++ 4th semester/Homework_7/RoundingBuilder/RoundingBuilder/RoundingBuilder.Tests.fs
@@ -0,0 +1,13 @@
+﻿module RoundingBuilder.Tests
+
+open Program
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+    let ``result rounded to thousandth`` () =
+        calculate 3 |> should equal 0.048
+
+[<Test>]
+    let ``result rounded to hundredths`` () =
+        calculate 2 |> should equal 0.05
