2e285bc800a25efe82b9c0501592019164ac3342
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-10-15 19:12:24 +0300

    Added intermediate versions of function

--- 4th semester/Homework_5/PointFreeFunction/PointFreeFunction/PointFreeFunction.Tests.fs
+++ 4th semester/Homework_5/PointFreeFunction/PointFreeFunction/PointFreeFunction.Tests.fs
@@ -7,8 +7,16 @@ open FsUnit
 [<Test>]
     let ``standart test`` () =
         pointFreeFunction 5 [1; 2; 3; 4] |> should equal [5; 10; 15; 20]
+        pointFreeFunction1 5 [1; 2; 3; 4] |> should equal [5; 10; 15; 20]
+        pointFreeFunction2 5 [1; 2; 3; 4] |> should equal [5; 10; 15; 20]
+        pointFreeFunction3 5 [1; 2; 3; 4] |> should equal [5; 10; 15; 20]
+        pointFreeFunction4 5 [1; 2; 3; 4] |> should equal [5; 10; 15; 20]
 
 [<Test>]
     let ``empty list test`` () =
         pointFreeFunction 5 [] |> should equal []
+        pointFreeFunction1 5 [] |> should equal []
+        pointFreeFunction2 5 [] |> should equal []
+        pointFreeFunction3 5 [] |> should equal []
+        pointFreeFunction4 5 [] |> should equal []
 
