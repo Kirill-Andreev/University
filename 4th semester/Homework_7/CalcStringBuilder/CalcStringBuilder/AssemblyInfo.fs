7181cc9d39d1753afa89af73463ca1a881eab6b3
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-11-14 00:13:41 +0300

    Add 7.2

---
+++ 4th semester/Homework_7/CalcStringBuilder/CalcStringBuilder/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace CalcStringBuilder.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// Общие сведения о сборке можно задать с помощью следующего 
+// набора атрибутов. Отредактируйте эти значения атрибутов, чтобы изменить сведения,
+// связанные с этой сборкой.
+[<assembly: AssemblyTitle("CalcStringBuilder")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("CalcStringBuilder")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// При установке значения False в атрибуте ComVisible типы в этой сборке становятся невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте для атрибута ComVisible значение True для этого типа.
+[<assembly: ComVisible(false)>]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект видим для COM
+[<assembly: Guid("f453c4ad-e3c1-42b3-a5f8-f5eedd637673")>]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+// 
+//       Основной номер версии
+//       Дополнительный номер версии 
+//       Номер сборки
+//      Редакция
+// 
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [<сборка: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ 4th semester/Homework_7/CalcStringBuilder/CalcStringBuilder/CalcStringBuilder.Tests.fs
@@ -0,0 +1,29 @@
+﻿module CalcStringBuilder.Tests
+
+open Program
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Addition of two numbers`` () = 
+    let result = 
+        stringExpr {
+            let! x = "1"
+            let! y = "2"
+            let z = x + y
+            return z
+        }
+    result |> should equal <| Some 3.0
+
+[<Test>]
+let ``Addition of number and not a number`` () = 
+    let result = 
+        stringExpr {
+            let! x = "1"
+            let! y = "f"
+            let z = x + y
+            return z
+        }
+    result |> should equal <| null
+
+
