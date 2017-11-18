2da8d3d05e0f7df49e7bb9db296a227e8583b1f2
Kirill-Andreev <andreev.kirill102@gmail.com>
2017-11-18 23:54:08 +0300

    Add 5.4

---
+++ 4th semester/Homework_5/LambdaInterpreter/LambdaInterpreter/LambdaInterpreter.Tests.fs
@@ -0,0 +1,20 @@
+﻿module LambdaInterpreter.Tests
+
+open Program
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+    let ``id test``() =
+        let term = (Abs ('x', Var 'x'))
+        betaReduction term |> should equal term
+
+[<Test>]
+    let ``free variables of (((λa.(λb.b b) (λb.b b)) b) ((λc.(c b)) (λa.a))) test``() =
+        let term = (App (App (Abs ('a', App (Abs ('b', App (Var 'b', Var 'b')), Abs ('b', App (Var 'b', Var 'b')))), Var 'b'),  App (Abs ('c', App (Var 'c', Var 'b')), Abs ('a', Var 'a'))))
+        free term |> should equal ['b'; 'b']
+
+[<Test>]
+    let ``SKKI test``() =
+        let term = betaReduction (App (App (Abs ('x', Abs ('y', Abs ('z', App (App (Var 'x', Var 'z'), App (Var 'y', Var 'z'))))), Abs ('x', Abs ('y', Var 'x'))),Abs ('x', Abs ('y', Var 'x'))))
+        betaReduction term |> should equal (Abs ('z',Var 'z'))
---
+++ 4th semester/Homework_5/LambdaInterpreter/LambdaInterpreter/Program.fs
@@ -0,0 +1,40 @@
+﻿module Program
+
+let alphabet = ['a' .. 'z']
+
+type Term =
+    | Var of char
+    | App of Term * Term
+    | Abs of char * Term
+
+let free term =
+    let rec aux term acc =
+        match term with
+        | Var x -> x::acc
+        | App (t1, t2) -> let fst = aux t1 acc
+                          let snd = aux t2 acc
+                          List.append fst snd
+        | Abs (x, t) -> let y = aux t acc
+                        List.filter (fun n -> n <> x) y
+    aux term []
+
+let rec substitute whereSubs insteadSubs whatSubs = 
+    match whereSubs with
+    | Var x when (x = insteadSubs) -> whatSubs
+    | Var x -> whereSubs
+    | App (t1, t2) -> App (substitute t1 insteadSubs whatSubs, substitute t2 insteadSubs whatSubs)
+    | Abs (x, t) -> 
+        match whatSubs with
+        | Var y when (x = insteadSubs) -> whereSubs
+        | _ when (not(List.contains x <| free whatSubs)) || (not(List.contains insteadSubs <| free t)) -> Abs(x, substitute t insteadSubs whatSubs)
+        | _ -> let y = List.head <| List.filter(fun n -> not(List.contains n <| List.append (free t) (free whatSubs))) alphabet
+               let fst = substitute t x <| Var y
+               let snd = substitute fst insteadSubs whatSubs
+               Abs (y, snd)
+
+let rec betaReduction term =
+    match term with
+    | Var x -> Var x
+    | App (Abs (x, t1), t2) -> betaReduction <| substitute t1 x t2
+    | App (t1, t2) -> App (betaReduction t1, betaReduction t2)
+    | Abs (x, t) -> Abs (x, betaReduction t)
